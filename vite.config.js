b96504c7f4cf50b222075e3941bbe5e6eb8ed59f
kallllllvv <haikaljkl34@gmail.com>
2025-11-23 01:58:38 +0700

    Percobaan Commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/panel_admin/',
+  plugins: [react()],
+  base: '/panel_admin/',        // ← WAJIB ditambah ini!
 })
