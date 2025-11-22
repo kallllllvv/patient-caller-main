99f161e7288d835df7e2cb89c7d19e03a40108a9
kallllllvv <haikaljkl34@gmail.com>
2025-11-22 17:29:23 +0700

    Percobaan commit

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/panel_admin">
       <Routes>
         <Route path="/" element={<Admin />} />
         <Route path="/display" element={<Display />} />
